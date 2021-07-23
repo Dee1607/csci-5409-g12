2989494e054509bca4d282ff213b711b692c4153
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-07-23 19:07:57 -0300

    Frontend Added

---
+++ reactModules/src/App.js
@@ -0,0 +1,42 @@
+/* Author: Team */
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <AppLayout>
+                        <Home />
+                    </AppLayout>
+                </Route>
+                <Route path="/login">
+                    <AppLayout>
+                        <Login />
+                    </AppLayout>
+                </Route>
+                <Route path="/register">
+                    <AppLayout>
+                        <Register />
+                    </AppLayout>
+                </Route>
+                <Route path="/dashboard">
+                    <DashboardLayout>
+                        <Dashboard />
+                    </DashboardLayout>
+                </Route>
+                <Route path="/">
+                    <div>404 Page not found.</div>
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
